e9afe2a7d6b2bb77d23e6a62cfa9c2f4126dbf32
CANIS-NAU <morgan.vigil-hayes@nau.edu>
2021-08-06 09:52:48 -0700

    Adding cs205_final_exam.sh

---
+++ cs205_final_exam.sh
@@ -0,0 +1,10 @@
+# TODO: Create a shell script that is able to use regular expressions and awk to goes through the file pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+# ======= SUMMARY OF POKEMON.DAT ======
+#    Total Pokemon: [VALUE]
+#    Avg. HP: [VALUE]
+#    Avg. Attack: [VALUE]
+# ======= END SUMMARY =======
+
+# The "Avg." values should be calculated as mean values for the corresponding columns.
+# The spacing and header formatting should match the above formatting description exactly.
+
