0e362578aa988b33ace21979d2c52d708d5da423
zw276 <116912688+zw276@users.noreply.github.com>
2022-11-08 21:25:58 -0700

    update

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -11,8 +11,4 @@
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
 
-BEGIN { HP=0 ; Atk=0 ; NR ; FILENAME ; NF} 
-
-{ HP+=$(NF-8) ; Atk+=$(NF-7) } 
-
-END { printf "# ===== SUMMARY OF DATA FILE =====\n#    File name: %s\n#    Total Pokemon: %d\n#    Avg. HP: %d\n#    Avg. Attack: %d\n# ===== END SUMMARY =====" FILENAME NR HP/NR Atk/NR}
+BEGIN { HP=0 ; Atk=0 ; NR ; FILENAME ; NF}  { HP+=$(NF-8) ; Atk+=$(NF-7) }  END { printf "# ===== SUMMARY OF DATA FILE =====\n#    File name: %s\n#    Total Pokemon: %d\n#    Avg. HP: %d\n#    Avg. Attack: %d\n# ===== END SUMMARY =====" FILENAME NR HP/NR Atk/NR}
